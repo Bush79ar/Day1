8cb3d0b6f21f7a77b080de72bfce7549acd1344b
Bush79ar <bb6112015@gmail.com>
2023-05-16 08:01:19 -0700

    Ex8

---
+++ 777.py
@@ -0,0 +1,32 @@
+'''
+x=[1,2,3]
+x.extend([4,5])
+print(x)
+'''
+
+'''
+x=['3','4','5']
+for i in x:
+    s="|".join(x)
+print(s)    
+'''
+'''
+result1 = [ i**2 for i in range(5) ]
+print(result1)
+
+result=[]
+for i in range(5):
+    result.append(i**2)
+print(result)
+'''
+'''
+result=[]
+for i in reage(5):
+    if i %2 == 0 :
+        result.append(i)
+print(result)
+
+result1 = [ i for i  in range(5)  if i %2==0]
+print(result1)
+'''
+
